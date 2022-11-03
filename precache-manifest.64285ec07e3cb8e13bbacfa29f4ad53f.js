fb9cc50bb1a79c12f8968236a23f8934131dc231
Claire Devlin <clairebeargoldielox@icloud.com>
2022-11-03 15:00:21 -0700

    Updates

---
+++ precache-manifest.64285ec07e3cb8e13bbacfa29f4ad53f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "223ff09c24577aafea39",
+    "url": "/cork-board-app/static/css/main.ef38f580.chunk.css"
+  },
+  {
+    "revision": "223ff09c24577aafea39",
+    "url": "/cork-board-app/static/js/main.223ff09c.chunk.js"
+  },
+  {
+    "revision": "044acfcabf655622e9a6",
+    "url": "/cork-board-app/static/js/1.044acfca.chunk.js"
+  },
+  {
+    "revision": "a280d5e079e06b9cfb9e",
+    "url": "/cork-board-app/static/js/runtime~main.a280d5e0.js"
+  },
+  {
+    "revision": "488e779e71d2b96e525dbc848c4ccbe2",
+    "url": "/cork-board-app/static/media/stickyNote.488e779e.png"
+  },
+  {
+    "revision": "284c93bd7b5c0dc9aaaeea474ebfbea9",
+    "url": "/cork-board-app/static/media/list.284c93bd.png"
+  },
+  {
+    "revision": "57d60c2ac5c92821a6e4494847b35fed",
+    "url": "/cork-board-app/static/media/tree.57d60c2a.png"
+  },
+  {
+    "revision": "b8bb92e29987cb609d748356bf32e82d",
+    "url": "/cork-board-app/index.html"
+  }
+];
