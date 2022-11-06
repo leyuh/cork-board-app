0c5ae1d2be093fa41b7736d182cd3f2bfeb1976b
Claire Devlin <clairebeargoldielox@icloud.com>
2022-11-06 12:12:53 -0800

    Disable small screen usage

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,12 +2,19 @@ import '../styles/Welcome.css';
 import React from 'react';
 
 function Welcome() {
-    return <div id="welcome-div">
-        <h1>Pinlo</h1>
-        <h2>BETA</h2>
-        <br/>
-        <h3>Create a board in the top left to get started.</h3>
-    </div>
+
+    if (window.innerWidth > 800) {
+        return <div id="welcome-div">
+            <h1>Pinlo</h1>
+            <h2>BETA</h2>
+            <br/>
+            <h3>Create a board in the top left to get started.</h3>
+        </div>
+    } else {
+        return <div id="welcome-div">
+            <h3>Sorry, Pinlo is not supported with this viewport. Check it out on a computer or tablet!</h3>
+        </div>
+    }
 }
 
 export default Welcome;
